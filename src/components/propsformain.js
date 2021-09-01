3237df2592e237bb28f8c9cf852bee751c2b6623
Marwan-Zakia <marwanamir.ma@gmail.com>
2021-09-01 14:34:17 +0300

    lab03solve

--- src/components/propsformain.js
+++ src/components/propsformain.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-
 class Hornedbeast extends React.Component {
 
   constructor(props) {
@@ -17,6 +16,7 @@ incrementNumberOfvotes = () => {
     this.setState({
         numberOfvotes : this.state.numberOfvotes + 1
     });
+    this.props.changeToshow(this.props.data.title);
 };
 
   render() {
@@ -29,16 +29,15 @@ incrementNumberOfvotes = () => {
     <Card.Text>
     {this.props.data.keyword}
     </Card.Text>
-    <Card.Text>
-    {this.props.data.description}
-    </Card.Text>
     <Card.Text> horns :
     {this.props.data.horns}
     </Card.Text>
     <Card.Text>
     💖  : {this.state.numberOfvotes}
     </Card.Text>
-    <Button variant="primary" onClick ={this.props.changeToshow} >Go somewhere</Button>
+    <Button variant="secondary" onClick={this.incrementNumberOfvotes} >
+              FIND OUT MORE ....
+            </Button>
   </Card.Body>
 </Card>
 
